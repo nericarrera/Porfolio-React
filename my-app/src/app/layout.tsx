af39d87dc5d8572eb9522ae05c84f7ce010c3e6b
nericarrera <nericarrera@yahoo.com.ar>
2025-07-18 17:30:01 -0300

    ok

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="es" className="scroll-smooth">
+       <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className="bg-gray-100 text-gray-900 min-h-screen">
         <Navbar />
         <main className="max-w-7xl mx-auto px-4">
